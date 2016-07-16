7d447ce7124860b63636e8c0819ebe7c12541abf
Tomas Herceg <tomas.herceg@riganti.cz>
2016-07-16 21:53:10 +0200

    Exception additional data providers

---
+++ src/Infrastructure/Riganti.Utils.Infrastructure.Services/Logging/CultureAdditionalDataProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace Riganti.Utils.Infrastructure.Services.Logging
+{
+    public class CultureAdditionalDataProvider : IAdditionalDataProvider
+    {
+        public void ExtractAdditinalData(IDictionary<string, string> additionalData)
+        {
+            additionalData["CurrentCulture"] = Thread.CurrentThread.CurrentCulture.DisplayName;
+            additionalData["CurrentUICulture"] = Thread.CurrentThread.CurrentUICulture.DisplayName;
+        }
+    }
+}
