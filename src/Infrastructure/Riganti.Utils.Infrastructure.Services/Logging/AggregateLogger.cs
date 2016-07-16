8e2f9b1a70fe9d3b94d3ace9de29fe694e8a59ac
Tomas Herceg <tomas.herceg@riganti.cz>
2016-07-16 21:58:26 +0200

    AggregateLogger added

---
+++ src/Infrastructure/Riganti.Utils.Infrastructure.Services/Logging/AggregateLogger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Riganti.Utils.Infrastructure.Services.Logging
+{
+    public class AggregateLogger : ILogger 
+    {
+        private readonly ILogger[] loggers;
+
+        public AggregateLogger(params ILogger[] loggers)
+        {
+            this.loggers = loggers;
+        }
+
+        public void LogException(Exception exception, IDictionary<string, string> additionalData = null, Severity severity = Severity.Error)
+        {
+            foreach (var logger in loggers)
+            {
+                logger.LogException(exception, additionalData, severity);
+            }
+        }
+
+        public void LogMessage(string message, IDictionary<string, string> additionalData = null, Severity severity = Severity.Info)
+        {
+            foreach (var logger in loggers)
+            {
+                logger.LogMessage(message, additionalData, severity);
+            }
+        }
+    }
+}
