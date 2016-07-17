a4b25e73be17209dcecb08b25d4946c7701fbade
Tomas Herceg <tomas.herceg@riganti.cz>
2016-07-17 10:30:48 +0200

    FirstLevelQuery objects added

---
+++ src/Infrastructure/Riganti.Utils.Infrastructure.Core/ISoftDeleteEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Riganti.Utils.Infrastructure.Core
+{
+    /// <summary>
+    /// Represents an entity which is deleted by setting the DeletedDate property to current date.
+    /// </summary>
+    public interface ISoftDeleteEntity
+    {
+
+        DateTime? DeletedDate { get; set; }
+
+    }
+}
