e31afda04811b748988aaa33a89d2e616e2fbfcf
Tomáš Herceg <herceg@vbnet.cz>
2016-07-24 19:57:44 +0200

    AutoMapper library implemented

---
+++ src/Infrastructure/Riganti.Utils.Infrastructure.AutoMapper/EntityDTOMapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Riganti.Utils.Infrastructure.Services.Facades;
+
+namespace Riganti.Utils.Infrastructure.AutoMapper
+{
+    public class EntityDTOMapper<TEntity, TDTO> : IEntityDTOMapper<TEntity, TDTO>
+    {
+        public TDTO MapToDTO(TEntity source)
+        {
+            return Mapper.Map<TDTO>(source);
+        }
+
+        public void PopulateEntity(TDTO source, TEntity target)
+        {
+            Mapper.Map(source, target);
+        }
+    }
+    
+}
