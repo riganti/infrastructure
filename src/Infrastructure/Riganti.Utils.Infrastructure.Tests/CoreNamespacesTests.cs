6c95601d3549d1353a3068aa86763f18c17c0293
Martin Dybal <martin@dybal.it>
2016-10-31 13:03:42 +0100

    Move files to folder. DO NOT MOVE NAMESPACE

---
+++ src/Infrastructure/Riganti.Utils.Infrastructure.Tests/CoreNamespacesTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Riganti.Utils.Infrastructure.Core;
+
+namespace Riganti.Utils.Infrastructure.Tests
+{
+    [TestClass]
+    public class CoreNamespacesTests
+    {
+        /// <summary>
+        /// If this test fail you have to correct the name space. 
+        /// If you are using Resharper you should set NamespaceProvider to false in folder properties 
+        /// </summary>
+        [TestMethod]
+        public void AllClassesHaveCorrectNameSpace_Test()
+        {
+            var correctNameSpace = "Riganti.Utils.Infrastructure.Core";
+            var infrastructureCoreAssembly = typeof(IRepository<,>).Assembly;
+
+            var incorrectTypes = infrastructureCoreAssembly.GetTypes()
+                                                          .Where(t => t.Namespace != correctNameSpace)
+                                                          .Select(t => t.FullName)
+                                                          .ToArray();
+
+            Assert.IsFalse(incorrectTypes.Any(), $"Incorect types: {string.Join(", ", incorrectTypes)}");
+        }
+    }
+}
