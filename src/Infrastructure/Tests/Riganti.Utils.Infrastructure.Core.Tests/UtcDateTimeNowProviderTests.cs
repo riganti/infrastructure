189204621c47c1b2d3b010a4d164abb3ac12be6e
Martin Dybal <martin@dybal.it>
2016-11-02 16:25:12 +0100

    UtcDateTimeNowProvider provides UTC time test

---
+++ src/Infrastructure/Tests/Riganti.Utils.Infrastructure.Core.Tests/UtcDateTimeNowProviderTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xunit;
+
+namespace Riganti.Utils.Infrastructure.Core.Tests
+{
+    public class UtcDateTimeNowProviderTests
+    {
+        readonly UtcDateTimeNowProvider utcDateTimeNowProviderSUT = new UtcDateTimeNowProvider();
+
+        [Fact]
+        public void ProvidesUtcTime_Test()
+        {
+            var utcDateTimeNowProviderValue = utcDateTimeNowProviderSUT.Now;
+
+            Assert.True(DateTime.Compare(utcDateTimeNowProviderValue, DateTime.UtcNow) == 0);
+        }
+    }
+}
