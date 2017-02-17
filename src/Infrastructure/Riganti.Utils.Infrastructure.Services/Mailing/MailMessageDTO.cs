54ae149b947d4d29ec89b1471c9bc7b49740caa3
Michal Altair Valasek <michal.valasek@altairis.cz>
2017-02-17 17:28:12 +0100

    Added Sender and custom headers

--- src/Infrastructure/Riganti.Utils.Infrastructure.Services/Mailing/MailMessageDTO.cs
+++ src/Infrastructure/Riganti.Utils.Infrastructure.Services/Mailing/MailMessageDTO.cs
@@ -1,11 +1,12 @@
 using System.Collections.Generic;
+using System.Collections.Specialized;
 
-namespace Riganti.Utils.Infrastructure.Services.Mailing
-{
-    public class MailMessageDTO
-    {
+namespace Riganti.Utils.Infrastructure.Services.Mailing {
+    public class MailMessageDTO {
 
         public MailAddressDTO From { get; set; }
+
+        public MailAddressDTO Sender { get; set; }
 
         public ICollection<MailAddressDTO> To { get; } = new List<MailAddressDTO>();
 
@@ -14,6 +15,8 @@ namespace Riganti.Utils.Infrastructure.Services.Mailing
         public ICollection<MailAddressDTO> Bcc { get; } = new List<MailAddressDTO>();
 
         public ICollection<MailAddressDTO> ReplyTo { get; } = new List<MailAddressDTO>();
+
+        public ICollection<KeyValuePair<string, string>> CustomHeaders { get; set; } = new List<KeyValuePair<string, string>>();
 
         public string Subject { get; set; }
 
