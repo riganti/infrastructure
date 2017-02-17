0b3f470c2cdf2a72c1d13abc43e2792beeaaec65
Michal Altair Valasek <michal.valasek@altairis.cz>
2017-02-17 17:29:09 +0100

    Implemented PickupFolderMailSender using MimeKit

---
+++ src/Infrastructure/Riganti.Utils.Infrastructure.Services.Smtp/Extensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MimeKit;
+using Riganti.Utils.Infrastructure.Services.Mailing;
+
+namespace Riganti.Utils.Infrastructure.Services.Smtp {
+    internal static class Extensions {
+
+        public static MimeMessage ToMimeMessage(this MailMessageDTO m) {
+            var msg = new MimeMessage();
+
+            // Add standard header fields
+            msg.From.Add(m.From.ToMailboxAddress());
+            msg.To.AddRange(m.To.ToMailboxAddress());
+            msg.Cc.AddRange(m.Cc.ToMailboxAddress());
+            msg.Bcc.AddRange(m.Bcc.ToMailboxAddress());
+            msg.Sender = m.Sender.ToMailboxAddress();
+            msg.ReplyTo.AddRange(m.ReplyTo.ToMailboxAddress());
+            msg.Subject = m.Subject;
+
+            // Add custom header fields
+            foreach (var item in m.CustomHeaders) {
+                msg.Headers.Add(item.Key, item.Value);
+            }
+
+            // Construct body
+            var bb = new BodyBuilder {
+                TextBody = m.BodyText,
+                HtmlBody = m.BodyHtml
+            };
+
+            // Add attachments
+            foreach (var item in m.Attachments) {
+                ContentType ct;
+                var r = ContentType.TryParse(item.MimeType, out ct);
+                if (!r) ct = new ContentType("application", "octet-stream");
+                bb.Attachments.Add(item.Name, item.Stream, ct);
+            }
+
+            msg.Body = bb.ToMessageBody();
+
+            return msg;
+        }
+
+        internal static IEnumerable<MailboxAddress> ToMailboxAddress(this IEnumerable<MailAddressDTO> a) {
+            return a.Select(x => x.ToMailboxAddress());
+        }
+
+        internal static MailboxAddress ToMailboxAddress(this MailAddressDTO a) {
+            if (a == null) return null;
+            return new MailboxAddress(a.DisplayName, a.Address);
+        }
+
+    }
+}
---
+++ src/Infrastructure/Riganti.Utils.Infrastructure.Services.Smtp/PickupFolderMailSender.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using MimeKit;
+using Riganti.Utils.Infrastructure.Services.Mailing;
+
+namespace Riganti.Utils.Infrastructure.Services.Smtp {
+    public class PickupFolderMailSender : IMailSender {
+
+        public string FolderName { get; }
+
+        public PickupFolderMailSender(string folderName) {
+            this.FolderName = folderName;
+        }
+
+        public async Task SendAsync(MailMessageDTO message) {
+            if (string.IsNullOrWhiteSpace(this.FolderName)) throw new InvalidOperationException("FolderName property value cannot be empty or whitespace only string.");
+
+            // Convert to message
+            var msg = message.ToMimeMessage();
+
+            // Write to temp file to avoid pickup of incomplete message
+            var tempFileName = Path.GetTempFileName();
+            using (var sw = File.CreateText(tempFileName)) {
+                // Write envelope sender
+                await sw.WriteLineAsync($"X-Sender: <{message.From.Address}>");
+
+                // Write envelope receivers
+                var receivers = message.To
+                    .Union(message.Cc)
+                    .Union(message.Bcc);
+                foreach (var item in receivers.Select(x => x.Address)) {
+                    await sw.WriteLineAsync($"X-Receiver: <{item}>");
+                }
+
+                // Write rest of message
+                msg.WriteTo(sw.BaseStream);
+            }
+
+            // Move the file to final destination
+            var msgFileName = Path.Combine(this.FolderName, string.Join(".", DateTime.Now.ToString("yyyyMMdd-HHmmss"), Guid.NewGuid().ToString("N"), "eml"));
+            File.Move(tempFileName, msgFileName);
+        }
+    }
+}
