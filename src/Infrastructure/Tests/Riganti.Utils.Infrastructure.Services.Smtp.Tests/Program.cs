9a044c1a7b94dbe890a9afcd5bb84fd30f8b6643
Michal Altair Valasek <michal.valasek@altairis.cz>
2017-02-17 22:51:20 +0100

    Added tests for PickupFolderMailSender

---
+++ src/Infrastructure/Tests/Riganti.Utils.Infrastructure.Services.Smtp.Tests/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Riganti.Utils.Infrastructure.Services.Smtp.Tests {
+    public class Program {
+        public static void Main(string[] args) {
+        }
+    }
+}
