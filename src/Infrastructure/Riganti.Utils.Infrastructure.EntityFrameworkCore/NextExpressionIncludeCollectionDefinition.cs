d1acf74ec1c04bd7a9d876f479ec0ff8d368eed7
Tomas Jurasek <tomas.jurasek@riganti.cz>
2017-06-27 12:51:41 +0200

    NextExpressionIncludeCollectionDefinition added

---
+++ src/Infrastructure/Riganti.Utils.Infrastructure.EntityFrameworkCore/NextExpressionIncludeCollectionDefinition.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using Riganti.Utils.Infrastructure.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Riganti.Utils.Infrastructure.EntityFrameworkCore
+{
+    public class NextExpressionIncludeCollectionDefinition<TEntity, TResultList, TResult, TNextResult> : IChainableIncludeDefinition<TEntity, TNextResult>, IIncludeDefinition<TEntity>
+      where TEntity : class
+      where TResultList : class, IEnumerable<TResult>
+      where TResult : class
+      where TNextResult : class
+
+    {
+        private readonly IChainableIncludeDefinition<TEntity, TResultList> baseInclude;
+
+        public Expression<Func<TResult, TNextResult>> Expression { get; private set; }
+
+        public NextExpressionIncludeCollectionDefinition(IChainableIncludeDefinition<TEntity, TResultList> baseInclude, Expression<Func<TResult, TNextResult>> expression)
+        {
+            Expression = expression;
+            this.baseInclude = baseInclude;
+        }
+
+        IQueryable<TEntity> IIncludeDefinition<TEntity>.ApplyInclude(IQueryable<TEntity> query)
+        {
+            var baseQuery = ((IIncludeDefinition<TEntity>)baseInclude).ApplyInclude(query);
+            return ((IIncludableQueryable<TEntity, TResultList>)baseQuery).ThenInclude(Expression);
+        }
+    }
+}
