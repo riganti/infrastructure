6f009576e5c133d32725f6b518c06b6e3de1e8ad
luboshl <luboshl>
2017-08-17 11:35:52 +0200

    Added mapping from DTO to Entity.

--- src/Infrastructure/Riganti.Utils.Infrastructure.Services/Facades/IEntityDTOMapper.cs
+++ src/Infrastructure/Riganti.Utils.Infrastructure.Services/Facades/IEntityDTOMapper.cs
@@ -1,11 +1,11 @@
 namespace Riganti.Utils.Infrastructure.Services.Facades
 {
-    public interface IEntityDTOMapper<in TEntity, TDTO>
+    public interface IEntityDTOMapper<TEntity, TDTO>
     {
-
         TDTO MapToDTO(TEntity source);
 
-        void PopulateEntity(TDTO source, TEntity target);
+        TEntity MapToEntity(TDTO source);
 
+        void PopulateEntity(TDTO source, TEntity target);
     }
 }
