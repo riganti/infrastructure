e1059c7e1d15e9eb9f5058ac49624f19a06cc77a
Lubos Hladik <github@hlsoft.cz>
2017-10-11 16:54:21 +0200

    Added failing tests.

--- src/Infrastructure/Tests/Riganti.Utils.Infrastructure.Services.Tests/Facades/TemporalRelationshipCrudFacadeTests.cs
+++ src/Infrastructure/Tests/Riganti.Utils.Infrastructure.Services.Tests/Facades/TemporalRelationshipCrudFacadeTests.cs
@@ -104,6 +104,75 @@ namespace Riganti.Utils.Infrastructure.Services.Tests.Facades
             Assert.True(newRelationship.ValidityBeginDate < DateTime.Now);
         }
 
+        [Theory]
+        //[InlineData(100)]
+        [InlineData(901)]
+        public void Delete_ByDto_InvalidesItem(int employeeProjectId)
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<EmployeeProjectDbContext>()
+                .UseInMemoryDatabase(databaseName: "EmployeesAndProjects" + Guid.NewGuid().ToString().Substring(10))
+                .Options;
+            PrepareInMemoryDbContext(options);
+            var facade = GetFacade(options);
+            EmployeeProject employeeProject;
+            // get the EmployeeProject
+            using (var ctx = new EmployeeProjectDbContext(options))
+            {
+                employeeProject = ctx.EmployeeProjects.First(ep => ep.Id == employeeProjectId);
+            }
+            var employeeId = employeeProject.EmployeeId;
+            var projectId = employeeProject.ProjectId;
+            var employeeProjectDTO = Mapper.Map<EmployeeProjectDTO>(employeeProject);
+
+            // Act
+            facade.Delete(employeeProjectDTO);
+
+            // Assert
+            List<EmployeeProject> employeeProjects;
+            using (var ctx = new EmployeeProjectDbContext(options))
+            {
+                employeeProjects = ctx.EmployeeProjects.Where(ep => ep.EmployeeId == employeeId && ep.ProjectId == projectId).ToList();
+            }
+
+            // all items has been invalidated
+            Assert.True(employeeProjects.All(ep => ep.ValidityEndDate < DateTime.Now));
+        }
+
+        [Theory]
+        [InlineData(100)]
+        [InlineData(901)]
+        public void Delete_ById_InvalidesItem(int employeeProjectId)
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<EmployeeProjectDbContext>()
+                .UseInMemoryDatabase(databaseName: "EmployeesAndProjects" + Guid.NewGuid().ToString().Substring(10))
+                .Options;
+            PrepareInMemoryDbContext(options);
+            var facade = GetFacade(options);
+            EmployeeProject employeeProject;
+            // get the EmployeeProject
+            using (var ctx = new EmployeeProjectDbContext(options))
+            {
+                employeeProject = ctx.EmployeeProjects.First(ep => ep.Id == employeeProjectId);
+            }
+            var employeeId = employeeProject.EmployeeId;
+            var projectId = employeeProject.ProjectId;
+
+            // Act
+            facade.Delete(employeeProject.Id);
+
+            // Assert
+            List<EmployeeProject> employeeProjects;
+            using (var ctx = new EmployeeProjectDbContext(options))
+            {
+                employeeProjects = ctx.EmployeeProjects.Where(ep => ep.EmployeeId == employeeId && ep.ProjectId == projectId).ToList();
+            }
+
+            // all items has been invalidated
+            Assert.True(employeeProjects.All(ep => ep.ValidityEndDate < DateTime.Now));
+        }
+
         private static EmployeeProjectsFacade GetFacade(DbContextOptions<EmployeeProjectDbContext> options)
         {
             Mapper.Initialize(cfg =>
@@ -128,6 +197,9 @@ namespace Riganti.Utils.Infrastructure.Services.Tests.Facades
             //        1       10              100
             //        2       10              101
             //        2       11              102
+            //        9       99              901
+            //        9       99              902
+            //        9       99              903
 
             var employeeProject100 = new EmployeeProject
             {
@@ -156,6 +228,33 @@ namespace Riganti.Utils.Infrastructure.Services.Tests.Facades
                 ValidityEndDate = DateTime.Today.AddDays(1)
             };
 
+            var employeeProject901 = new EmployeeProject
+            {
+                Id = 901,
+                EmployeeId = 9,
+                ProjectId = 99,
+                ValidityBeginDate = null,
+                ValidityEndDate = DateTime.Today.AddDays(-1).AddHours(-2)
+            };
+
+            var employeeProject902 = new EmployeeProject
+            {
+                Id = 902,
+                EmployeeId = 9,
+                ProjectId = 99,
+                ValidityBeginDate = DateTime.Today.AddDays(-1).AddHours(-2),
+                ValidityEndDate = DateTime.Today.AddDays(-1).AddHours(-1)
+            };
+
+            var employeeProject903 = new EmployeeProject
+            {
+                Id = 903,
+                EmployeeId = 9,
+                ProjectId = 99,
+                ValidityBeginDate = DateTime.Today.AddDays(-1).AddHours(-1),
+                ValidityEndDate = null
+            };
+
             var employees = new List<Employee>
             {
                 new Employee
@@ -167,6 +266,11 @@ namespace Riganti.Utils.Infrastructure.Services.Tests.Facades
                 {
                     Id = 2,
                     EmployeeProjects = new List<EmployeeProject> { employeeProject101, employeeProject102 }
+                },
+                new Employee
+                {
+                    Id = 9,
+                    EmployeeProjects = new List<EmployeeProject> { employeeProject901, employeeProject902, employeeProject903 }
                 }
             };
 
@@ -181,6 +285,11 @@ namespace Riganti.Utils.Infrastructure.Services.Tests.Facades
                 {
                     Id = 11,
                     EmployeeProjects = new List<EmployeeProject> { employeeProject102 }
+                },
+                new Project
+                {
+                    Id = 99,
+                    EmployeeProjects = new List<EmployeeProject> { employeeProject901, employeeProject902, employeeProject903 }
                 }
             };
 
@@ -191,6 +300,9 @@ namespace Riganti.Utils.Infrastructure.Services.Tests.Facades
                 ctx.EmployeeProjects.Add(employeeProject100);
                 ctx.EmployeeProjects.Add(employeeProject101);
                 ctx.EmployeeProjects.Add(employeeProject102);
+                ctx.EmployeeProjects.Add(employeeProject901);
+                ctx.EmployeeProjects.Add(employeeProject902);
+                ctx.EmployeeProjects.Add(employeeProject903);
                 ctx.SaveChanges();
             }
         }
