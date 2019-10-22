d0ab8ae4cdf0cbf6267ca5d52c1f886ebfcc8477
Michal Motyčka <michal.motycka@riganti.cz>
2019-10-22 17:14:35 +0200

    Updated all projects to .net standard 2.0. Fixed test.

--- src/Infrastructure/Tests/Riganti.Utils.Infrastructure.Core.Tests/DateTimeNowProvider/UtcDateTimeNowProviderTests.cs
+++ src/Infrastructure/Tests/Riganti.Utils.Infrastructure.Core.Tests/DateTimeNowProvider/UtcDateTimeNowProviderTests.cs
@@ -12,7 +12,14 @@ namespace Riganti.Utils.Infrastructure.Core.Tests.DateTimeNowProvider
         {
             var utcDateTimeNowProviderValue = utcDateTimeProviderSUT.Now;
 
-            Assert.True(DateTime.Compare(utcDateTimeNowProviderValue, DateTime.UtcNow) == 0);
+            Assert.True(AreDateTimeClose(DateTime.Now, utcDateTimeNowProviderValue));
+        }
+
+        public bool AreDateTimeClose(DateTime time, DateTime time2)
+        {
+            if (time.AddSeconds(-2) > time2 && time2 < time.AddSeconds(2))
+                return true;
+            return false;
         }
     }
 }
