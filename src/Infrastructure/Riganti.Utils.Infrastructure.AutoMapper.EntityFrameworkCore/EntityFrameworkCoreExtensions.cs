e56a48ad740909035ad1a3d678e849e1d4ab2eff
Luboš Hladík <lubos@hladikovi.net>
2022-03-08 10:15:33 +0100

    Cleanup csproj files.

--- src/Infrastructure/Riganti.Utils.Infrastructure.AutoMapper.EntityFrameworkCore/EntityFrameworkCoreExtensions.cs
+++ src/Infrastructure/Riganti.Utils.Infrastructure.AutoMapper.EntityFrameworkCore/EntityFrameworkCoreExtensions.cs
@@ -12,16 +12,15 @@ namespace Riganti.Utils.Infrastructure.AutoMapper
 {
     public static class EntityFrameworkCoreExtensions
     {
-
         public static void DropAndCreateCollection<TDbContext, TSource, TSourceItem, TDestination, TDestinationItem>
-            (
-                this IMemberConfigurationExpression<TSource, TDestination, ICollection<TDestinationItem>> config,
-                IEntityFrameworkUnitOfWorkProvider<TDbContext> unitOfWorkProvider,
-                Expression<Func<TSource, ICollection<TSourceItem>>> sourceCollectionSelector,
-                Func<TSourceItem, TDestinationItem> projection = null,
-                Action<TDestinationItem> removeCallback = null,
-                Func<TDestinationItem, bool> destinationFilter = null
-            )
+        (
+            this IMemberConfigurationExpression<TSource, TDestination, ICollection<TDestinationItem>> config,
+            IEntityFrameworkUnitOfWorkProvider<TDbContext> unitOfWorkProvider,
+            Expression<Func<TSource, ICollection<TSourceItem>>> sourceCollectionSelector,
+            Func<TSourceItem, TDestinationItem> projection = null,
+            Action<TDestinationItem> removeCallback = null,
+            Func<TDestinationItem, bool> destinationFilter = null
+        )
             where TDbContext : DbContext
             where TDestinationItem : class
         {
@@ -34,21 +33,22 @@ namespace Riganti.Utils.Infrastructure.AutoMapper
                 };
             }
 
-            Extensions.DropAndCreateCollection(config, sourceCollectionSelector, projection, removeCallback, destinationFilter);
+            Extensions.DropAndCreateCollection(config, sourceCollectionSelector, projection, removeCallback,
+                destinationFilter);
         }
 
 
         public static void SyncCollectionByKey<TDbContext, TSource, TSourceItem, TDestination, TDestinationItem>
-            (
-                this IMemberConfigurationExpression<TSource, TDestination, ICollection<TDestinationItem>> config,
-                IEntityFrameworkUnitOfWorkProvider<TDbContext> unitOfWorkProvider,
-                Expression<Func<TSource, ICollection<TSourceItem>>> sourceCollectionSelector,
-                Func<TSourceItem, TDestinationItem> createFunction = null,
-                Action<TSourceItem, TDestinationItem> updateFunction = null,
-                Action<TDestinationItem> removeFunction = null,
-                Func<TDestinationItem, bool> destinationFilter = null,
-                bool keepRemovedItemsInDestinationCollection = true
-            )
+        (
+            this IMemberConfigurationExpression<TSource, TDestination, ICollection<TDestinationItem>> config,
+            IEntityFrameworkUnitOfWorkProvider<TDbContext> unitOfWorkProvider,
+            Expression<Func<TSource, ICollection<TSourceItem>>> sourceCollectionSelector,
+            Func<TSourceItem, TDestinationItem> createFunction = null,
+            Action<TSourceItem, TDestinationItem> updateFunction = null,
+            Action<TDestinationItem> removeFunction = null,
+            Func<TDestinationItem, bool> destinationFilter = null,
+            bool keepRemovedItemsInDestinationCollection = true
+        )
             where TDbContext : DbContext
             where TDestinationItem : class
         {
@@ -75,33 +75,42 @@ namespace Riganti.Utils.Infrastructure.AutoMapper
 
             if (sourceKeyType == null || destinationKeyType == null || sourceKeyType != destinationKeyType)
             {
-                throw new InvalidOperationException("The source and destination collection items must implement the IEntity<TKey> interface and the keys must be equal!");
+                throw new InvalidOperationException(
+                    "The source and destination collection items must implement the IEntity<TKey> interface and the keys must be equal!");
             }
 
             var sourceParam = Expression.Parameter(typeof(TSourceItem), "i");
-            var sourceKeySelector = Expression.Lambda(Expression.Property(sourceParam, nameof(IEntity<int>.Id)), sourceParam);
+            var sourceKeySelector =
+                Expression.Lambda(Expression.Property(sourceParam, nameof(IEntity<int>.Id)), sourceParam);
             var destinationParam = Expression.Parameter(typeof(TDestinationItem), "i");
-            var destinationKeySelector = Expression.Lambda(Expression.Property(destinationParam, nameof(IEntity<int>.Id)), destinationParam);
+            var destinationKeySelector =
+                Expression.Lambda(Expression.Property(destinationParam, nameof(IEntity<int>.Id)), destinationParam);
 
-            var method = typeof(Extensions).GetTypeInfo().GetMethod("SyncCollectionByKeyReflectionOnly", BindingFlags.NonPublic | BindingFlags.Static);
-            method.MakeGenericMethod(typeof(TSource), typeof(TSourceItem), typeof(TDestination), typeof(TDestinationItem), sourceKeyType)
-                .Invoke(null, new object[] { config, sourceCollectionSelector, sourceKeySelector, destinationKeySelector, createFunction, updateFunction, removeFunction, keepRemovedItemsInDestinationCollection, destinationFilter });
+            var method = typeof(Extensions).GetTypeInfo().GetMethod("SyncCollectionByKeyReflectionOnly",
+                BindingFlags.NonPublic | BindingFlags.Static);
+            method.MakeGenericMethod(typeof(TSource), typeof(TSourceItem), typeof(TDestination),
+                    typeof(TDestinationItem), sourceKeyType)
+                .Invoke(null,
+                    new object[] {
+                        config, sourceCollectionSelector, sourceKeySelector, destinationKeySelector, createFunction,
+                        updateFunction, removeFunction, keepRemovedItemsInDestinationCollection, destinationFilter
+                    });
         }
 
 
         public static void SyncCollectionByKey<TDbContext, TSource, TSourceItem, TDestination, TDestinationItem, TKey>
-            (
-                this IMemberConfigurationExpression<TSource, TDestination, ICollection<TDestinationItem>> config,
-                IEntityFrameworkUnitOfWorkProvider<TDbContext> unitOfWorkProvider,
-                Expression<Func<TSource, ICollection<TSourceItem>>> sourceCollectionSelector,
-                Func<TSourceItem, TKey> sourceKeySelector,
-                Func<TDestinationItem, TKey> destinationKeySelector,
-                Func<TSourceItem, TDestinationItem> createFunction = null,
-                Action<TSourceItem, TDestinationItem> updateFunction = null,
-                Action<TDestinationItem> removeFunction = null,
-                Func<TDestinationItem, bool> destinationFilter = null,
-                bool keepRemovedItemsInDestinationCollection = true
-            )
+        (
+            this IMemberConfigurationExpression<TSource, TDestination, ICollection<TDestinationItem>> config,
+            IEntityFrameworkUnitOfWorkProvider<TDbContext> unitOfWorkProvider,
+            Expression<Func<TSource, ICollection<TSourceItem>>> sourceCollectionSelector,
+            Func<TSourceItem, TKey> sourceKeySelector,
+            Func<TDestinationItem, TKey> destinationKeySelector,
+            Func<TSourceItem, TDestinationItem> createFunction = null,
+            Action<TSourceItem, TDestinationItem> updateFunction = null,
+            Action<TDestinationItem> removeFunction = null,
+            Func<TDestinationItem, bool> destinationFilter = null,
+            bool keepRemovedItemsInDestinationCollection = true
+        )
             where TDbContext : DbContext
             where TDestinationItem : class
         {
@@ -114,8 +123,9 @@ namespace Riganti.Utils.Infrastructure.AutoMapper
                 };
             }
 
-            Extensions.SyncCollectionByKey(config, sourceCollectionSelector, sourceKeySelector, destinationKeySelector, createFunction, updateFunction, removeFunction, keepRemovedItemsInDestinationCollection, destinationFilter);
+            Extensions.SyncCollectionByKey(config, sourceCollectionSelector, sourceKeySelector, destinationKeySelector,
+                createFunction, updateFunction, removeFunction, keepRemovedItemsInDestinationCollection,
+                destinationFilter);
         }
-
     }
 }
